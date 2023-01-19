959eebecf33a9de094175d7efd8635f476bc7d1d
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2023-01-19 12:14:44 -0800

    updated my logo

---
+++ component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Header() {
+  return (
+    <div class="flex flex-col justify-center items-center -mt-8">
+    <img src="/logo.png" className="max-w-sm scale-50 -mt-20 -mb-10" alt="" />
+</div>
+  )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Hero from '../component/Hero'
+import Header from '../component/Header'
 
 
 
 export default function Home() {
   return (
    <div className='bg-gradient-to-r from-cyan-50 to-slate-50'>
+    <Header></Header>
 <Hero/>
    </div>
   )
